86d9a6c0ffac02e55a584fe95946834265059f63
Bennett Benedict <bennybenester@gmail.com>
2022-05-06 11:09:54 +0300

    create pr if message has commit

--- src/action.js
+++ src/action.js
@@ -25,8 +25,43 @@ async function run() {
       break;
   }
 }
+const createorupdatepr = async ({ branch, owner, repo, body, full_name }) => {
+  try {
+    const existing_pr = await octokit.rest.pulls.list({
+      owner,
+      repo,
+      state: 'open',
+      head: owner + ':' + branch,
+      base: DESTINATION_BRANCH,
+    });
+    if (existing_pr?.data?.length === 0) {
+      // create new pr
+      const createpr = await octokit.request(`POST /repos/${full_name}/pulls`, {
+        owner,
+        repo,
+        title: branch,
+        body,
+        head: branch,
+        base: DESTINATION_BRANCH,
+      });
+      return createpr;
+    } else {
+      // update existing pr
+      const updatepr = await octokit.rest.pulls.update({
+        pull_number: existing_pr?.data[0].number,
+        owner,
+        repo,
+        title: branch,
+        body,
+        head: branch,
+        base: DESTINATION_BRANCH,
+      });
+      return updatepr;
+    }
+  } catch (e) {}
+};
 const checkCompareCommits = async ({ head, owner, full_name, repo }) => {
-  const { commits } = await octokit.request(
+  let { commits } = await octokit.request(
     `GET /repos/${full_name}/compare/${DESTINATION_BRANCH}...${head}`,
     {
       owner,
@@ -41,17 +76,20 @@ const checkCompareCommits = async ({ head, owner, full_name, repo }) => {
     return;
   }
 
-  (commits || []).forEach((e, i) => {
-    if (
-      !e?.commit?.message.includes('Merge') &&
-      !e?.commit?.message.includes('Merged') &&
-      !e?.commit?.message.includes('skip') &&
-      !e?.commit?.message.includes('Skip')
-    )
-      commits =
-        i === 0
-          ? '> ' + e.commit.message
-          : commits + '\n\n' + '> ' + e.commit.message;
+  commits = (commits || [])
+    .map((e, i) => {
+      return i === 0 ? '> ' + e.commit.message : e.commit.message;
+    })
+    .join('\n\n' + '> ');
+
+  console.log('COMMITS', commits);
+
+  await createorupdatepr({
+    branch: head,
+    owner,
+    repo,
+    full_name,
+    body: commits,
   });
 };
 const pr = async () => {
